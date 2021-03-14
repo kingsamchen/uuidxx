388fc28985829a3bcd626030756aae50678f4e4d
Kingsley Chen <kingsamchen@gmail.com>
2021-03-14 21:20:39 +0800

    Setup project layout

---
+++ uuidxx/uuid.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2021 Kingsley Chen <kingsamchen@gmail.com>
+// This file is subject to the terms of license that can be
+// found in the LICENSE file.
+
+#include "uuidxx/uuid.h"
+
+namespace uuid {
+
+}   // namespace uuid
---
+++ uuidxx/uuid.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 Kingsley Chen <kingsamchen@gmail.com>
+// This file is subject to the terms of license that can be
+// found in the LICENSE file.
+
+#ifndef UUIDXX_UUID_H_
+#define UUIDXX_UUID_H_
+
+namespace uuid {
+
+}   // namespace uuid
+
+#endif  // UUIDXX_UUID_H_

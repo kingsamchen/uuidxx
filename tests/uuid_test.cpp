f6262559943ee1f931e4388340fd88b8d367bdba
Kingsley Chen <kingsamchen@gmail.com>
2021-03-21 12:33:18 +0800

    Add uniqueness test for v4 generation

--- tests/uuid_test.cpp
+++ tests/uuid_test.cpp
@@ -3,13 +3,17 @@
 
 #include "uuidxx/uuidxx.h"
 
+#include <algorithm>
+#include <string>
+#include <vector>
+
 namespace {
 
 }   // namespace
 
 namespace uuid {
 
-TEST_CASE("Generation of uuid v4", "[v4]")
+TEST_CASE("Format compliant", "[v4]")
 {
     auto uuid = make_v4();
 
@@ -18,6 +22,19 @@ TEST_CASE("Generation of uuid v4", "[v4]")
     auto s = uuid.to_string();
     REQUIRE(s.size() == 36);
     REQUIRE(s[14] == '4');
+}
+
+TEST_CASE("Uniquess", "[v4]")
+{
+    std::vector<std::string> ids;
+    for (int i = 0; i < 10; ++i) {
+        ids.push_back(make_v4().to_string());
+    }
+
+    std::sort(ids.begin(), ids.end());
+
+    auto it = std::adjacent_find(ids.begin(), ids.end());
+    REQUIRE(it == ids.end());
 }
 
 }   // namespace uuid

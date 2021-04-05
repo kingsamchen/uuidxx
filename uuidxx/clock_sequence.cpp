5a15b2f78beb1d929c788d00fe17cd07584d05c9
Kingsley Chen <kingsamchen@gmail.com>
2021-04-05 23:14:05 +0800

    Add clock_sequence for generating time-based uuid

---
+++ uuidxx/clock_sequence.cpp
@@ -0,0 +1,51 @@
+// Copyright (c) 2021 Kingsley Chen <kingsamchen@gmail.com>
+// This file is subject to the terms of license that can be
+// found in the LICENSE file.
+
+#include "uuidxx/clock_sequence.h"
+
+#include <random>
+
+namespace uuid {
+
+clock_sequence::clock_sequence()
+    : last_time_(0),
+      seq_(static_cast<uint16_t>(std::random_device{}()))
+{}
+
+// static
+clock_sequence& clock_sequence::instance()
+{
+    static clock_sequence instance;
+    return instance;
+}
+
+// static
+uint64_t clock_sequence::get_timestamp_since_epoch()
+{
+    auto ts = k_epoch_diff + std::chrono::duration<clock_sequence::duration>(
+        std::chrono::system_clock::now().time_since_epoch());
+    return ts.count();
+}
+
+std::tuple<uint64_t, uint16_t> clock_sequence::read()
+{
+    uint64_t now = get_timestamp_since_epoch();
+    uint16_t seq;
+
+    {
+        std::lock_guard lock(mtx_);
+
+        // The clock is set backward or read too fast.
+        if (now <= last_time_) {
+            ++seq_;
+        }
+
+        last_time_ = now;
+        seq = seq_;
+    }
+
+    return std::make_tuple(now, seq);
+}
+
+}   // namespace uuid
---
+++ uuidxx/clock_sequence.h
@@ -0,0 +1,51 @@
+// Copyright (c) 2021 Kingsley Chen <kingsamchen@gmail.com>
+// This file is subject to the terms of license that can be
+// found in the LICENSE file.
+
+#ifndef UUIDXX_CLOCK_SEQUENCE_H_
+#define UUIDXX_CLOCK_SEQUENCE_H_
+
+#include <chrono>
+#include <mutex>
+
+namespace uuid {
+
+class clock_sequence {
+private:
+    // 100ns intervals.
+    using duration = std::chrono::duration<uint64_t, std::ratio<1, 10'000'000>>;
+
+public:
+    ~clock_sequence() = default;
+
+    clock_sequence(const clock_sequence&) = delete;
+
+    clock_sequence(clock_sequence&&) = delete;
+
+    clock_sequence& operator=(const clock_sequence&) = delete;
+
+    clock_sequence& operator=(clock_sequence&&) = delete;
+
+    static clock_sequence& instance();
+
+    std::tuple<uint64_t, uint16_t> read();
+
+private:
+    clock_sequence();
+
+    // Gets 100ns interval count since UUID epoch.
+    static uint64_t get_timestamp_since_epoch();
+
+private:
+    std::mutex mtx_;
+    uint64_t last_time_;
+    uint16_t seq_;
+
+    // Difference in 100ns intervals between UUID epoch (1582/10/15 00:00:00) and Unix
+    // epoch (1970/01/01 00:00:00)
+    static constexpr duration k_epoch_diff{122192928000000000};
+};
+
+}   // namespace uuid
+
+#endif  // UUIDXX_CLOCK_SEQUENCE_H_
